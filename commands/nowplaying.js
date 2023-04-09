54216975c1e54c56ec256b256f025dbafb4a85c4
miawbot <107809540+miawbot@users.noreply.github.com>
2023-04-09 18:05:17 +0700

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -30,7 +30,7 @@ module.exports = {
 > By: <@${track.user.id}>`);
 
       embed.setTimestamp();
-      embed.setFooter({ text: `MusicMaker ❤️` })
+      embed.setFooter({ text: `Miiawww ❤️` })
 
       const saveButton = new ButtonBuilder();
       saveButton.setLabel(lang.msg47);
